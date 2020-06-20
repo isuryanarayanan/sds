5ce74c5c6058c964b0672cd5720d4b994ffb61cd
devdoodan <devdoodan@gmail.com>
2020-06-20 11:46:05 +0530

    admin form complete

--- backend/project/engine/engine.py
+++ backend/project/engine/engine.py
@@ -1,8 +1,7 @@
 from django.core.exceptions import ValidationError
 
 
-def check_overlap(self, fixed_start, fixed_end, new_start, new_end):
-    print("the fuck")
+def check_overlap(fixed_start, fixed_end, new_start, new_end):
     overlap = False
     if new_start == fixed_end or new_end == fixed_start:
         overlap = False
@@ -15,18 +14,14 @@ def check_overlap(self, fixed_start, fixed_end, new_start, new_end):
 
 
 def checkTestsForSlots(cal):
-    elm = 0
-    for i in range(0, len(cal)):
+    res = True
 
-        if cal[i] != cal[elm]:
-            if cal[i].end <= cal[i].start:
-                raise ValidationError('Ending times must after starting times')
-
-            if cal[elm].date == cal[i].date:
-                if check_overlap(cal[i].start, cal[i].end, cal[elm].start, cal[elm].end):
-                    raise ValidationError('There is an overlap with another event: ' + str(
-                        cal[i].day) + ', ' + str(cal[i].start) + '-' + str(cal[i].end))
-
-        elm = elm+1
-
-    return True
+    for xcal in cal:
+        if xcal.end < xcal.start:
+            res = False
+        for ycal in cal:
+            if xcal != ycal:
+                if xcal.day == ycal.day:
+                    if check_overlap(xcal.start, xcal.end, ycal.start, ycal.end):
+                        res = False
+    return res
