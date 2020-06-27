ee51cca0d314edaf23c67733103fa5b831a8267d
devdoodan <devdoodan@gmail.com>
2020-06-27 17:48:35 +0530

    event

--- backend/project/engine/models/Event.py
+++ backend/project/engine/models/Event.py
@@ -1,13 +1,14 @@
 from django.db import models
-from accounts.models.profiles.customer import customer_profile
-from accounts.models.profiles.vendor import vendor_profile
 # Create your models here.
 
 
 class Event(models.Model):
 
-    customer = models.ForeignKey(customer_profile, on_delete=models.CASCADE)
-    vendor = models.ForeignKey(vendor_profile, on_delete=models.CASCADE)
+    customer = models.ForeignKey(
+        "accounts.customer_profile", on_delete=models.CASCADE)
+    vendor = models.ForeignKey(
+        "accounts.vendor_profile", on_delete=models.CASCADE)
+    datetime = models.ForeignKey("engine.TimeSlot", on_delete=models.CASCADE)
 
     class Meta:
         app_label = "engine"
