d9a00bdb10ca8cdf3232fd061698f456d291df65
isuryanarayanan <a.suryanarayanan2000@gmail.com>
2020-07-12 06:14:13 +0530

    CreateUserEngine

--- backend/project/accounts/api/v1/create_user.py
+++ backend/project/accounts/api/v1/create_user.py
@@ -4,11 +4,39 @@ from rest_framework.permissions import IsAuthenticated
 import json
 
 
+class CreateUserEngine():
+
+    request = ''  # The incoming request.
+    response = ''  # The response to return to the view.
+    utype = ''  # The user type to create
+
+    def __init__(self, params):
+        # Loading defaults
+        self.utype = json.loads(params.body)['user_type']
+        self.request = params
+
+        getattr(self, self.utype.upper())()
+
+    def respond(self):  # Respond from request.
+        return str(self.response)
+
+    def CUSTOMER(self):
+        pass
+
+    def VENDOR(self):
+        pass
+
+    def ADMINISTRATOR(self):
+        # Since it's a protected view
+        # Check for auth.
+        pass
+
+
 class CreateUserView(APIView):
     permission_classes = ()
 
     def post(self, request):
-        content = json.loads(request.body)
-        # Create users here
-
-        return Response(content)
+        # Create the engine.
+        Engine = CreateUserEngine(request)
+        # Respond.
+        return Response(Engine.respond())
--- backend/project/accounts/api/v1/urls.py
+++ backend/project/accounts/api/v1/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path, include
 from .create_user import CreateUserView
+from .handshake import HandshakeView
 
 urlpatterns = [
-    path('hello/', CreateUserView.as_view(), name='hello'),
+    path('create_user/', CreateUserView.as_view(), name='CreateUserView'),
+    path('handshake/', HandshakeView.as_view(), name='HandshakeView')
 ]
