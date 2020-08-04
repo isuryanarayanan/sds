181df94bf1ef7c62db288375731b4a92be8a19c8
devdoodan <devdoodan@gmail.com>
2020-08-04 22:08:54 +0530

    frontend

---
+++ frontend/src/views/GetStarted.vue
@@ -0,0 +1,128 @@
+<template>
+  <div class="get-started p-5">
+    <div class="get-started-wrapper row">
+      <div class="signup p-1 col-sm-6">
+        <div class="py-3">
+          <div class="su-brand font1">
+            Create a new account
+          </div>
+          <div class="su-form">
+            <form>
+              <div class="form-group px-3">
+                <label for="su-email">Email address</label>
+                <input
+                  type="email"
+                  class="form-control"
+                  id="su-email"
+                  aria-describedby="emailHelp"
+                  placeholder="Enter email"
+                />
+                <small id="emailHelp" class="form-text text-muted"
+                  >We'll never share your email with anyone else.</small
+                >
+              </div>
+              <div class="form-group px-3">
+                <label for="su-password1">Password</label>
+                <input
+                  type="password"
+                  class="form-control"
+                  id="su-password1"
+                  placeholder="Password"
+                />
+              </div>
+              <div class="form-group px-3">
+                <label for="su-password2">Password Confirmation</label>
+                <input
+                  type="password"
+                  class="form-control"
+                  id="su-password2"
+                  placeholder="Re-enter your password"
+                />
+              </div>
+              <div class="p-3">
+                <div class="form-group form-check">
+                  <input
+                    type="checkbox"
+                    class="form-check-input"
+                    id="su-agreement"
+                  />
+                  <label class="form-check-label" for="su-agreement"
+                    >Check me out</label
+                  >
+                </div>
+                <button type="submit" class="btn btn-primary">Submit</button>
+              </div>
+            </form>
+          </div>
+        </div>
+      </div>
+      <div class="login p-1 col-sm-6">
+        <div class="py-3">
+          <div class="li-brand font1">
+            Login to an existing account
+          </div>
+          <div class="li-form">
+            <form>
+              <div class="form-group px-3">
+                <label for="li-email">Email address</label>
+                <input
+                  type="email"
+                  class="form-control"
+                  id="li-email"
+                  aria-describedby="emailHelp"
+                  placeholder="Enter email"
+                />
+              </div>
+              <div class="form-group px-3">
+                <label for="li-password1">Password</label>
+                <input
+                  type="password"
+                  class="form-control"
+                  id="li-password1"
+                  placeholder="Password"
+                />
+              </div>
+
+              <div class="p-3">
+                <div class="form-group form-check">
+                  <input
+                    type="checkbox"
+                    class="form-check-input"
+                    id="li-cache-login"
+                  />
+                  <label class="form-check-label" for="li-cache-login"
+                    >Keep me signed in</label
+                  >
+                </div>
+                <button type="submit" class="btn btn-primary">Submit</button>
+              </div>
+            </form>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss" scoped>
+.signup {
+  :hover .su-brand {
+    color: #42b983;
+  }
+}
+.login {
+  :hover .li-brand {
+    color: #42b983;
+  }
+}
+
+.su-brand,
+.li-brand {
+  font-size: 32px;
+  color: #2c3e50;
+}
+</style>
---
+++ frontend/src/views/Home.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="home">
+    <div class="home-wrapper">
+      <div class="brand p-5 text-center">
+        <h1 class="display-1 font1">Social Distancing Scheduler</h1>
+      </div>
+      <div class="sub-brand text-center px-5">
+        <p class="font2 px-5">
+          Social Distancing Scheduler is a standard to schedule your contacts
+          and avoid contracting viruses like COVID-19. Built using Python, with
+          the Django framework for the backend and Vue js for the frontend. SDS
+          allows its users to see how many people have registered to be present
+          at a public space at a given time. SDS is not a product or service it
+          is a standard.
+        </p>
+      </div>
+      <div class="call-to-action text-center">
+        <router-link to="/get-started" class="text-light px-1">
+          <div class="btn btn-dark btn-lg shadow">Get Started</div>
+        </router-link>
+        <a
+          href="https://github.com/devdoodan/social-distancing-scheduler"
+          class="text-light px-1"
+        >
+          <div class="btn btn-secondary shadow">Contribute</div>
+        </a>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+// import HelloWorld from "@/components/HelloWorld.vue";
+
+export default {
+  name: "Home",
+  components: {},
+};
+</script>
+
+<style lang="scss" scoped>
+.home-wrapper {
+  width: 100%;
+  .sub-brand {
+    font-size: 24px;
+  }
+}
+</style>
