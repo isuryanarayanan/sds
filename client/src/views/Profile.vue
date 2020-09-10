ef10b71d17b1037af31eae1760092176370c1636
devdoodan <devdoodan@gmail.com>
2020-09-10 19:35:12 +0530

    poopoo

--- client/src/views/Profile.vue
+++ client/src/views/Profile.vue
@@ -1,15 +1,22 @@
 <template>
   <div class="profile p-5">
-    <v-title class="display-1 p-2">
-      {{ $store.getters["user/get_user"]._USERNAME }}</v-title
-    >
+    <v-card>
+      <div class="header  font1">
+        {{ $store.getters["user/get_user"]._USERNAME }}'s Profile
+      </div>
+    </v-card>
+    <!--
+    	<v-title class="display-1 p-2">
+      		{{ $store.getters["user/get_user"]._USERNAME }}
+    	</v-title>
 
-    <v-btn>Edit<v-icon small>fas fa-edit</v-icon></v-btn>
-    <v-title class="display-1 p-2">
-      {{ $store.getters["user/get_user"]._EMAIL }}</v-title
-    >
+    	<v-btn>Edit<v-icon small>fas fa-edit</v-icon></v-btn>
+    	<v-title class="display-1 p-2">
+     		{{ $store.getters["user/get_user"]._EMAIL }}
+    	</v-title>
 
-    <v-btn>Edit<v-icon small>fas fa-edit</v-icon></v-btn>
+    	<v-btn>Edit<v-icon small>fas fa-edit</v-icon></v-btn>
+    	-->
   </div>
 </template>
 
